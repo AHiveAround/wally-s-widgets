ca09d65952eb6ba8c243ac0f41e6a5f937cdaad9
James <jamesp@fisheyehq.com>
2022-03-18 13:33:07 +0000

    refactor size variable

--- app/Services/PackService.php
+++ app/Services/PackService.php
@@ -47,23 +47,23 @@ class PackService
             return null;
         }
 
-        foreach ($packSizes as $key => $size) {
-            $qty = floor($remainingPacks / $size);
-            $remainingPacks = $remainingPacks % $size;
+        foreach ($packSizes as $key => $packSize) {
+            $qty = floor($remainingPacks / $packSize);
+            $remainingPacks = $remainingPacks % $packSize;
 
             if ($key === (count($packSizes) - 2)) {
-                if ($size > $remainingPacks && $remainingPacks > $packSizes->get(count($packSizes) - 1)) {
+                if ($packSize > $remainingPacks && $remainingPacks > $packSizes->get(count($packSizes) - 1)) {
                     $remainingPacks = 0;
                     $qty++;
                 }
             }
 
-            if ($remainingPacks > 0 && $size === $packSizes->last()) {
+            if ($remainingPacks > 0 && $packSize === $packSizes->last()) {
                 $qty++;
             }
 
             if ($qty) {
-                $requiredPackSizes[] = ['size' => $size, 'qty' => $qty];
+                $requiredPackSizes[] = ['size' => $packSize, 'qty' => $qty];
             }
         }
 
